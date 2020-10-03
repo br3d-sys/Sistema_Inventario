da0989eaa0066f21e276c9fdab3937cee4c27139
Branco <branco_937@hotmail.com>
2020-10-03 14:13:49 -0500

    Se creo la clase categoria

---
+++ Inventario/Web_Farmacia/Clases/Categoria.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Farmacia.Clases
+{
+    public class Categoria
+    {
+        private int id_categoria;
+        private string nombre;
+        private string descripcion;
+        private int disponibilidad;
+
+        public int Id_categoria
+        {
+            get
+            {
+                return id_categoria;
+            }
+
+            set
+            {
+                id_categoria = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public int Disponibilidad
+        {
+            get
+            {
+                return disponibilidad;
+            }
+
+            set
+            {
+                disponibilidad = value;
+            }
+        }
+    }
+}
