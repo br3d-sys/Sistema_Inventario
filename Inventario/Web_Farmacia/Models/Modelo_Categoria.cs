dce9b1f28ae05f7c3f4161d30ced8f55147ab6ef
Cristhian <cristhian_tuctoleon@hotmail.com>
2020-10-04 14:17:38 -0500

    Modelos

---
+++ Inventario/Web_Farmacia/Models/Modelo_Categoria.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using Web_Farmacia.Clases;
+
+namespace Web_Farmacia.Models
+{
+  
+
+    public class Modelo_Categoria
+    {
+        MySqlCommand cmd;
+        MySqlConnection con;
+
+        public Modelo_Categoria()
+        {
+
+        }
+        public Boolean guardar(Categoria cate)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_C_Tabla_Categoria";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_nombre", cate.Nombre);
+                        cmd.Parameters.AddWithValue("_descripcion", cate.Descripcion);
+                        cmd.Parameters.AddWithValue("_disponibilidad", cate.Disponibilidad);
+
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public List<Categoria> listar()
+        {
+            try
+            {
+                MySqlDataReader rd;
+                List<Categoria> lista = new List<Categoria>();
+
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_A_Tabla_Categoria";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        rd = cmd.ExecuteReader();
+
+                        while (rd.Read())
+                        {
+                            lista.Add(new Categoria
+                            {
+                                Id_categoria = rd.GetInt32("id_categoria"),
+                                Nombre = rd.GetString("nombre"),
+                                Descripcion = rd.GetString("descripcion"),
+                                Disponibilidad = rd.GetString("disponibilidad"),
+                            });
+                        }
+
+                        rd.Close();
+
+                    }
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Boolean actualizar(Categoria cate)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+
+                        cmd.CommandText = "SP_M_Tabla_Categoria";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_nombre", are.Nombre);
+                        cmd.Parameters.AddWithValue("_descripcion", are.Descripcion);
+                        cmd.Parameters.AddWithValue("_disponibilidad", are.Disponibilidad);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public Boolean eliminar(int id)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_E_Tabla_Categoria";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_id_categoria", id);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
---
+++ Inventario/Web_Farmacia/Models/Modelo_Invetario.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Farmacia.Models
+{
+    public class Modelo_Invetario
+    {
+        MySqlCommand cmd;
+        MySqlConnection con;
+
+        public Modelo_Invetario()
+        {
+
+        }
+        public Boolean guardar(Inventario inven)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_C_Tabla_Inventario";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_id_bien", inven.Id_bien);
+                        cmd.Parameters.AddWithValue("_inventario", inven.Inventario);
+                        cmd.Parameters.AddWithValue("_fecha", inven.Fecha);
+                        cmd.Parameters.AddWithValue("_estado", inven.Estado);
+                        cmd.Parameters.AddWithValue("_disponibilidad", are.Disponibilidad);
+
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public List<Invetario> listar()
+        {
+            try
+            {
+                MySqlDataReader rd;
+                List<Inventario> lista = new List<Inventario>();
+
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_A_Tabla_Inventario";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        rd = cmd.ExecuteReader();
+
+                        while (rd.Read())
+                        {
+                            lista.Add(new Inventario
+                            {
+                                Id_bien = rd.GetInt32("id_bien"),
+                                Inventario = rd.GetString("inventario"),
+                                Fecha = rd.GetString("fecha"),
+                                Estado = rd.GetString("estado"),
+                                Disponibilidad = rd.GetString("disponibilidad"),
+                            });
+                        }
+
+                        rd.Close();
+
+                    }
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Boolean actualizar(Inventario inven)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+
+                        cmd.CommandText = "SP_M_Tabla_Inventario";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_id_bien", are.Id_Bien);
+                        cmd.Parameters.AddWithValue("_inventario", are.Inventario);
+                        cmd.Parameters.AddWithValue("_fecha", are.Fecha);
+                        cmd.Parameters.AddWithValue("estado", are.Estado);
+                        cmd.Parameters.AddWithValue("_disponibilidad", are.Disponibilidad);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public Boolean eliminar(int id)
+        {
+            try
+            {
+                using (con = Conexion.conectar())
+                {
+                    using (cmd = new MySqlCommand())
+                    {
+                        cmd.CommandText = "SP_E_Tabla_Inventario";
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Connection = con;
+
+                        cmd.Parameters.AddWithValue("_id_inventario", id);
+
+                        if (cmd.ExecuteNonQuery() > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
